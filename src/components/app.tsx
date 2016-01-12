22a6cf93651978d4924e98994a6593a81d32116e
Daniel Trinh <daniel.s.trinh@gmail.com>
2016-01-11 22:41:57 -0800

    update webpack and get rid of unused fetch comment

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -8,13 +8,13 @@ import { connect } from 'react-redux'
 import { CounterType } from '../actions'
 import Counter from './counter'
 import * as UI from 'material-ui'
+
 export const NICE = 'pink';
 export const SUPER_NICE = 'darkred';
 
 export class App extends Component<any, any> {
     constructor(props: any) {
         super(props);
-
         Store.store.dispatch({
             type: Actions.CounterType.INSERT,
             counter: Store.CounterRecordClass({
@@ -39,7 +39,6 @@ export class App extends Component<any, any> {
     }
 
     render(): JSX.Element {
-        // var wut = fetch("http://services.runescape.com/m=itemdb_rs/bestiary/bestiaryNames.json?letter=Y").then(x => console.log(x.status))
         return (
             <UI.List subheader="Counters">
                 <Counter counterId="1"/>
