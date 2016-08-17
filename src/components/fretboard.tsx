2410ecb14d298fba4666f4d78d2f422b1e467c46
Daniel Trinh <daniel.s.trinh@gmail.com>
2016-08-17 12:58:48 -0700

    minor tweaks to fretboard

--- src/components/fretboard.tsx
+++ src/components/fretboard.tsx
@@ -32,10 +32,18 @@ export class Fretboard extends Component<FretboardProps, any> {
         let fretboard = [];
         for (let i of _.range(0, this.props.frets)) {
             let slots = [];
-            for (let j of _.range(0, 7)) {
-                slots.push(<FretSlot key={`j${j}fret`} height={25} width={65}/>);
+
+            let sideFretHeight = 10;
+            for (let j of _.range(0, 6)) {
+                let height = 25;
+                if (j == 0) {
+                    height = sideFretHeight;
+                }
+                slots.push(<FretSlot key={`j${j}fret`} height={height} width={65}/>);
                 slots.push(<div key={`j${j}string`} style={stringStyle(j)}/>)
             }
+            slots.push(<FretSlot key={`j${7}fret`} height={sideFretHeight-3} width={65}/>);
+
             fretboard.push(<div key={`i${i}fretrow`} style={floatStyle}>{slots}</div>)
         }
         return <div style={fretRow}>
@@ -83,6 +91,7 @@ interface FretSlotProps {
 export class FretSlot extends Component<FretSlotProps, any> {
     height: number;
     width: number;
+    boardColor: string =  "#d9b38c";
 
     constructor(props) {
         super(props);
@@ -96,15 +105,27 @@ export class FretSlot extends Component<FretSlotProps, any> {
             boxShadow: "3px 0px 0px 0px #ccc"
         };
     }
+
     fretSectionStyle() {
         return {
             width: 65,
             marginRight: 5,
             height: this.fretSectionStyle,
-            background: "#d9b38c"
+            background: this.boardColor
         };
     }
 
+    fretSectionStyleCircle() {
+        return {
+            width: 120,
+            height: 120,
+            background: "#7fee1d",
+            fontSize: 16,
+            color: "#fff",
+            textAlign:"center",
+            borderRadius: 60
+        };
+    }
 
     render(): JSX.Element {
         return <div>
